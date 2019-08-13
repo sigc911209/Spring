1dd0fcb696d06ce26eb4ac6fac37c93b07367d34
carlos <sigc911209@users.noreply.github.com>
2019-08-12 21:40:25 -0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "archivo de comentarios";
+
+//linea uno de comentarios
+
+//linea dos de comentarios
+
+//linea tres de comentarios
+
+//linea cuatro de comentarios
+
+//linea cinco de comentarios
+
+//linea 6 de comandos
+
+
+?>
